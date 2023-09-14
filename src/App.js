a8c004fc9e9b6a3abb0fc9ce2eb48e145f5c8faf
unknown <christiansevilla0007@gmail.com>
2023-09-14 08:47:15 +0800

    Frontend

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, {Component} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { notification } from "antd";
+import { ACCESS_TOKEN } from "./constant";
+import Login from "./components/common/Login";
+import Registration from "./components/common/register";
+import LoadingIndicator from "./components/common/LoadingIndicator";
+
+class App extends Component {
+  // constructor(props) {
+  // super(props);
+  //   this.state = {
+  //     currentUser: null,
+  //     isAuthenticated: false,
+  //     isLoading: true,
+  //   };
+  //   this.handleLogout = this.handleLogout.bind(this);
+  //   this.loadCurrentUser = this.loadCurrentUser.bind(this);
+  //   this.handleLogin = this.handleLogin.bind(this);
+
+  //   notification.config({
+  //     placement: "topRight",
+  //     top: 70,
+  //     duration: 3,
+  //   });
+  // }
+
+  // componentDidMount() {
+  //   this.loadCurrentUser();
+  // }
+
+  // handleLogout(
+  //   redirectTo = "/",
+  //   notificationType = "success",
+  //   description = "You're successfully logged out."
+  // ) {
+  //   localStorage.removeItem(ACCESS_TOKEN);
+
+  //   this.setState({
+  //     currentUser: null,
+  //     isAuthenticated: false,
+  //   });
+
+  //   this.props.history.push(redirectTo);
+
+  //   notification[notificationType]({
+  //     message: "JUMPSTART",
+  //     description: description,
+  //   });
+  // }
+
+  // handleLogin() {
+  //   notification.success({
+  //     message: "JUMPSTART",
+  //     description: "You're successfully logged in.",
+  //   });
+  //   this.loadCurrentUser();
+  //   this.props.history.push("/");
+  // }
+
+  render() {
+    // if (this.state.isLoading) {
+    //   return <LoadingIndicator/>;
+    // }
+    return (
+     <Router>
+      <Routes>
+      <Route path="/login" element={<Login onLogin={this.handleLogin} />} />
+      <Route path="/register" element={<Registration />} />
+
+      </Routes>
+     </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/constant/index.js
@@ -0,0 +1,8 @@
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+export const ACCESS_TOKEN = 'accessToken';
+export const PHONE_NUMBER = 'phoneNumber';
+export const EMAIL = 'email';
+export const ID = 'userId';
+export const FIRST_NAME = 'firstName';
+export const ROLE_NAME = 'roleName';
+export const MEMBER_NUMBER = 'memberNumber';
